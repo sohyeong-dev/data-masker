3a9d1cff0987def8ce1c7eee03aea0c8205dc80c
isohyeong <so-hyeong@naver.com>
2024-07-29 22:24:58 +0900

    first commit: Spring Boot project setup

---
+++ src/test/java/com/fastcampus/innercircle/data_masker/DataMaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.innercircle.data_masker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
